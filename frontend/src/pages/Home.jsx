abe1177021b56f344cad6e2056e0afe898362b90
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-09-24 21:28:31 +0100

    commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import LastestCollection from '../components/LastestCollection'
+import BestSeller from '../components/BestSeller'
+import OurPolicy from '../components/OurPolicy'
 
 const Home = () => {
   return (
     <div>
       <Hero/>
       <LastestCollection/>
+      <BestSeller/>
+      <OurPolicy/>
     </div>
   )
 }
