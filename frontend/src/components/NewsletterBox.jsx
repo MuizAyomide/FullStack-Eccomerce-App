d1cb24f608602bf8117492eddaf864c54c923653
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-09-25 10:33:15 +0100

    commit

--- frontend/src/components/NewsletterBox.jsx
+++ frontend/src/components/NewsletterBox.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
 
 const NewsletterBox = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+  };
 
-export default NewsletterBox
+  return (
+    <div className="text-center">
+      <p className="text-2xl font-medium text-gray-800">
+        Subscribe Now and Get 20% Off
+      </p>
+      <p className="text-gray-400 mt-3">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint voluptas
+        dolorem eos fugiat, consequuntur architecto.
+      </p>
+      <form
+        onSubmit={onSubmitHandler}
+        className="w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6  border pl-3"
+      >
+        <input
+          className="w-full sm:flex-1 outline-none"
+          type="email"
+          placeholder="Enter your email"
+          required
+        />
+        <button
+          type="submit"
+          className="bg-black text-white text-xs px-10 py-4"
+        >
+          Subscribe
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default NewsletterBox;
