4b4b3709eea3780709a958016bc8ddad164334ab
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-09-27 22:48:43 +0100

    commit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -55,6 +55,7 @@ const Navbar = () => {
             <img onClick={()=>setVisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt="" />
         </div>
       {/* sidebar Menu for smaller screen  */}
+      
       <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? `w-full` : `w-0`}`}>
         <div className="flex flex-col text-gray-600 ">
             <div onClick={()=>setVisible(false)} className="flex items-center gap-4 p-3 cursor-pointer">
