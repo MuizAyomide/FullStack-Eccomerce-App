c2dcb8956c5c7e3128de43a77d259c69baa4bd15
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-09-27 22:49:33 +0100

    commit

--- frontend/src/components/ProductItems.jsx
+++ frontend/src/components/ProductItems.jsx
@@ -1,20 +1,27 @@
-import React, { useContext } from 'react'
-import { ShopContext } from '../context/ShopContext'
-import { Link } from 'react-router-dom'
+import React, { useContext } from "react";
+import { ShopContext } from "../context/ShopContext";
+import { Link } from "react-router-dom";
 
-const ProductItems = ({id, image, price, name}) => {
-
-const {currency} = useContext(ShopContext)
+const ProductItems = ({ id, image, price, name }) => {
+  const { currency } = useContext(ShopContext);
 
   return (
-    <Link className='text-gray-700 cursor-pointer' to={`/product/${id}`}>
-        <div className="overflow-hidden">
-            <img className='hover:scale-110 transition ease-in-out' src={image[0]} alt="" />
-        </div>
-        <p className='pt-3 pd-1 text-sm'>{name}</p>
-        <p className='text-sm font-medium'> {currency}{price}</p>
+    <Link className="text-gray-700 cursor-pointer" to={`/product/${id}`}>
+      <div className="overflow-hidden">
+        <img
+          className="hover:scale-110 transition ease-in-out"
+          src={image[0]}
+          alt=""
+        />
+      </div>
+      <p className="pt-3 pd-1 text-sm">{name}</p>
+      <p className="text-sm font-medium">
+        {" "}
+        {currency}
+        {price}
+      </p>
     </Link>
-  )
-}
+  );
+};
 
-export default ProductItems
+export default ProductItems;
