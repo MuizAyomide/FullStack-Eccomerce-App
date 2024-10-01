c24c79b6bcc5938aec6cb5d7ddaae67b0d381643
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-10-01 11:10:56 +0100

    commit

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 export const ShopContext = createContext();
 
@@ -11,6 +12,7 @@ const ShopContextProvider = (props) => {
   const [search, setSearch] = useState("");
   const [showSearch, setShowSearch] = useState(false);
   const [cartItems, setCartItems] = useState({});
+  const navigate = useNavigate()
 
   const addToCart = async (itemId, size) => {
 if (!size){
@@ -58,11 +60,11 @@ if (!size){
 
   }
 
-  const getCartAmount = async ()=> {
+  const getCartAmount = ()=> {
     let totalAmount = 0;
 
     for(const items in cartItems){
-      let itemInfo = products.find((product)=> product._id === item);
+      let itemInfo = products.find((product)=> product._id === items);
       for(const item in cartItems[items]){
         try {
           if (cartItems[items][item]> 0) {
@@ -74,7 +76,7 @@ if (!size){
         }
       }
     }
-    return totalAmount
+    return totalAmount;
   }
 
 
@@ -90,8 +92,8 @@ if (!size){
     addToCart,
     getCartCount,
     updateQuantity,
-    totalAmount,
-    getCartAmount
+    getCartAmount,
+    navigate
   };
 
 
