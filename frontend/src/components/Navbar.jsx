d4b2a8cc5e9cf6ac7dad739ff9866b6d9e9b2267
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-10-02 23:06:23 +0100

    commit

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -34,7 +34,7 @@ const Navbar = () => {
         </ul>
 
         <div className="flex items-center gap-6">
-            <img onClick={()=>setShowSearch(true)} src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
+        <Link to={'./collection'}>    <img onClick={()=>setShowSearch(true)} src={assets.search_icon} className='w-5 cursor-pointer' alt="" /></Link>
 
             <div className="group relative">
                 <img src={assets.profile_icon} className='w-5 cursor-pointer' alt="" />
