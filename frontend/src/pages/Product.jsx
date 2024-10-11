1b757c3dc07f30d2f21f2d4edb33e186e4a6a17a
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-10-11 22:41:08 +0100

    commit

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -6,10 +6,10 @@ import RelatedProducts from "../components/RelatedProducts";
 
 const Product = () => {
   const { productId } = useParams();
-  const { products, currency,addToCart } = useContext(ShopContext);
+  const { products, currency, addToCart } = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
   const [image, setImage] = useState("");
-  const [size,setSize] = useState('')
+  const [size, setSize] = useState("");
 
   const fetchProductData = async () => {
     products.map((item) => {
@@ -34,7 +34,7 @@ const Product = () => {
           <div className="flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full">
             {productData.image.map((item, index) => (
               <img
-              onClick={()=>setImage(item)}
+                onClick={() => setImage(item)}
                 src={item}
                 alt=""
                 key={index}
@@ -57,42 +57,79 @@ const Product = () => {
             <img src={assets.star_dull_icon} alt="" className="w-3 5" />
             <p className="pl-2">(122)</p>
           </div>
-              <p className="mt-5 text-3xl font-medium"> {currency}{productData.price}</p>
-              <p className="mt-5 text-gray-500 md:w-4/5">{productData.description}</p>
-              <div className="flex flex-col gap-4 my-8">
-                <p>
-                  Select Size
-                </p>
-                <div className="flex gap-2">{productData.sizes.map((item,index)=>(
-                  <button onClick={()=>setSize(item)} className={`border py-2 px-4 bg-gray-100 ${item === size ? 'border-orange-500 bg-orange-100': '' }`} key={index}>{item}</button>
-                ))}</div>
-              </div>
-              <button onClick={()=>addToCart(productData._id,size)} className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700">ADD TO CART</button>
-              <hr className="mt-8 sm:w-4/5"/>
-              <div className="text-sm text-gray-500 mt-5 flex flex-col gap-1">
-                <p className="capitalize">100% Original Product</p>
-                <p className="capitalize">Cash On Delivery is Available on this Product</p>
-                <p className="capitalize">Easy return and exchange policy within 7 days.</p>
-              </div>
+          <p className="mt-5 text-3xl font-medium">
+            {" "}
+            {currency}
+            {productData.price}
+          </p>
+          <p className="mt-5 text-gray-500 md:w-4/5">
+            {productData.description}
+          </p>
+          <div className="flex flex-col gap-4 my-8">
+            <p>Select Size</p>
+            <div className="flex gap-2">
+              {productData.sizes.map((item, index) => (
+                <button
+                  onClick={() => setSize(item)}
+                  className={`border py-2 px-4 bg-gray-100 ${
+                    item === size ? "border-orange-500 bg-orange-100" : ""
+                  }`}
+                  key={index}
+                >
+                  {item}
+                </button>
+              ))}
+            </div>
+          </div>
+          <button
+            onClick={() => addToCart(productData._id, size)}
+            className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700"
+          >
+            ADD TO CART
+          </button>
+          <hr className="mt-8 sm:w-4/5" />
+          <div className="text-sm text-gray-500 mt-5 flex flex-col gap-1">
+            <p className="capitalize">100% Original Product</p>
+            <p className="capitalize">
+              Cash On Delivery is Available on this Product
+            </p>
+            <p className="capitalize">
+              Easy return and exchange policy within 7 days.
+            </p>
+          </div>
         </div>
       </div>
 
-    {/* Description and review section */}
+      {/* Description and review section */}
 
-    <div className="mt-20">
-      <div className="flex">
-        <b className="border px-5 py-3 text-sm">Desctiption</b>
-        <p className="border px-5 py-3 text-sm">Review (122)</p>
+      <div className="mt-20">
+        <div className="flex">
+          <b className="border px-5 py-3 text-sm">Desctiption</b>
+          <p className="border px-5 py-3 text-sm">Review (122)</p>
+        </div>
+        <div className="flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500">
+          <p>
+            An e-commerce website is an online platform that facilitates the
+            buying and selling Lorem ipsum dolor, sit amet consectetur
+            adipisicing elit. Odit, aliquam. Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Enim assumenda asperiores quos magnam?
+            Obcaecati, autem.
+          </p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi
+            odit dolor, possimus qui ipsum esse dolorem labore modi repellat
+            amet atque voluptate, placeat ab deserunt odio animi ut ratione
+            assumenda.
+          </p>
+        </div>
       </div>
-      <div className="flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500">
-        <p >An e-commerce website is an online platform that facilitates the buying and selling Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit, aliquam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim assumenda asperiores quos magnam? Obcaecati, autem.</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi odit dolor, possimus qui ipsum esse dolorem labore modi repellat amet atque voluptate, placeat ab deserunt odio animi ut ratione assumenda.</p>
-      </div>
-    </div>
 
-    {/* display related products */}
+      {/* display related products */}
 
-    <RelatedProducts category={productData.category} subCategory={productData.subCategory}/>
+      <RelatedProducts
+        category={productData.category}
+        subCategory={productData.subCategory}
+      />
     </div>
   ) : (
     <div className="opacity-0"></div>
