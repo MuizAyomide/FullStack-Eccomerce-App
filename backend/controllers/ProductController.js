b83d05745e2d04c102ce52c217531fc7ac3d47aa
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-10-28 15:54:00 +0100

    commit

---
+++ backend/controllers/ProductController.js
@@ -0,0 +1,17 @@
+// Function for add product
+
+const addProduct = async (req, res) => {};
+
+// Function for list product
+
+const listProduct = async (req, res) => {};
+
+// Function for remove product
+
+const removeProduct = async (req, res) => {};
+
+// Function for single product info
+
+const singleProduct = async (req, res) => {};
+
+export { listProduct, addProduct, removeProduct, singleProduct };
