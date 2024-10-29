df941ecb14d27e8c1355142bc83ecc100b477aaf
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-10-29 23:51:38 +0100

    commit

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -8,6 +8,7 @@ const productRouter = express.Router();
 productRouter.post(
   "./add",
   upload.fields([
+    
     { name: "image1", maxCount: 1 },
     { name: "image2", maxCount: 1 },
     { name: "image3", maxCount: 1 },
