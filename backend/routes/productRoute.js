ab1f6d2ca43fd064a8cf01e2d1c1174bf25c30fc
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-10-29 01:20:03 +0100

    commit

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,11 +1,22 @@
-import express from "express"
+import express from "express";
 
-import { listProduct, addProduct, removeProduct, singleProduct } from '../controllers/ProductController'
+import { addProduct, listProduct, removeProduct, singleProduct } from '../controllers/ProductController';
+import upload from "../middleware/multer";
 
 const productRouter = express.Router();
 
-productRouter.post('./add', addProduct)
-productRouter.post('./remove', removeProduct)
-productRouter.post('./single', singleProduct)
-productRouter.get('./list', listProduct)
+productRouter.post(
+  "./add",
+  upload.fields([
+    { name: "image1", maxCount: 1 },
+    { name: "image2", maxCount: 1 },
+    { name: "image3", maxCount: 1 },
+    { name: "image4", maxCount: 1 },
+  ]),
+  addProduct
+);
+productRouter.post("./remove", removeProduct);
+productRouter.post("./single", singleProduct);
+productRouter.get("./list", listProduct);
 
+export default productRouter;
