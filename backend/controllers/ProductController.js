547fc5b343056e2b9d799eaf7d199d729809a349
muizAyomide <Akinrinolamuiz22@gmail.com>
2024-10-29 01:19:46 +0100

    commit

--- backend/controllers/ProductController.js
+++ backend/controllers/ProductController.js
@@ -1,17 +1,48 @@
 // Function for add product
 
-const addProduct = async (req, res) => {};
+const addProduct = async (req,res) => {
+    try {
+
+        const {name, description, price, category, subCategory, sizes, bestSeller} = req.body
+
+        const image1 = req.files.image1[0]
+        const image2 = req.files.image2[0]
+        const image3 = req.files.image3[0]
+        const image4 = req.files.image4[0]
+
+        console.log(name, description, price, category, subCategory, sizes, bestSeller)
+        console.log(image1, image2, image3, image4)
+
+        res.json({})
+
+
+        
+    } catch (error) {
+        console.log(error)
+            res.json({success: false, message: error.message})
+    }
+
+}
 
 // Function for list product
 
-const listProduct = async (req, res) => {};
+const listProduct = async (req,res) => {
+
+    
+}
 
 // Function for remove product
 
-const removeProduct = async (req, res) => {};
+const removeProduct = async (req,res) => {
+
+    
+}
 
 // Function for single product info
 
-const singleProduct = async (req, res) => {};
+const singleProduct = async (req,res) => {
 
-export { listProduct, addProduct, removeProduct, singleProduct };
+    
+}
+
+export {listProduct, addProduct, removeProduct, singleProduct}
